5119fa472ca00f785ee5f531d913368d039d9d1a
Demon <15315512115@163.com>
2019-01-03 17:41:41 +0800

    瀑布流

--- easy_pubuliu-master-m/fuild.js
+++ easy_pubuliu-master-m/fuild.js
@@ -1,3 +1,4 @@
+var currpage = 1, pagesize = 10, totalpage = 0;
 function $(el){
 	if(el.indexOf('#') > -1){
 		return document.getElementById(el.split('#')[1]);
@@ -22,20 +23,16 @@ function ajax(url, pagesize, currpage) {
 		if(xmlHttp.status == 200 && xmlHttp.readyState == 4){
 			console.log(JSON.parse(xmlHttp.response));
 			var data = JSON.parse(xmlHttp.response);
+			totalpage = data.totalpage;
 			appendDom(data.data);
 		}
 	}
-}
-ajax('http://127.0.0.1:9000/booklist', 10, 1);
-
-window.onload = function() {
-	calculate();
 }
 function calculate() {
 	var cols = document.body.clientWidth / $('li')[0].offsetWidth;
 	var arr = [];
 	for(var i = 0; i < $('li').length; i++ ){
-		if(i < cols){
+		if(i < cols-1){
 			arr.push($('li')[i].offsetHeight);
 		}else{
 			var minHeight = Math.min.apply(this, arr);
@@ -44,19 +41,43 @@ function calculate() {
 			$('li')[i].style.left = $('li')[getMindex(minHeight, arr)].offsetLeft + 'px';
 			arr[getMindex(minHeight, arr)] += $('li')[i].offsetHeight;
 		}
+		$('ul')[0].style.height = Math.max.apply(this, arr) + 'px';
 	}
+	
 }
 function appendDom(data) {
 	for(var i = 0; i < data.length; i++){
 		var oli = document.createElement('li');
-		oli.innerHTML = '<img src="'+data[i].bookCover+'" />';
+		var img = document.createElement('img');
+		var imgObj = new Image();
+		imgObj.src = data[i].bookCover;
+		imgObj.onload = function() {
+			calculate();
+		}
+		oli.appendChild(imgObj)
 		$('ul')[0].appendChild(oli);
 	}
 }
-reachBottom();
+
 function reachBottom() {
 	var clientHeight = document.documentElement.clientHeight;
-	var scrollTop = document.documentElement.scrollTop;
-	console.log(clientHeight, scrollTop);
+	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	var ulHeight = $('ul')[0].offsetHeight;
+	if(ulHeight-scrollTop <= clientHeight){
+		if(currpage < totalpage){
+			currpage++;
+			ajax('http://192.168.3.49:9000/booklist', pagesize, currpage);
+		}
+	}
+} 
+
+window.onload = function() {
+	ajax('http://192.168.3.49:9000/booklist', pagesize, currpage);
+//	calculate();
 }
+window.onscroll = function() {
+	reachBottom();
+	calculate();
+}
+
 
